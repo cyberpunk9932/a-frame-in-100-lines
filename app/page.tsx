395cf4f770b0d487ac43f3430e0b636053e4f267
cyberpunk9932 <70695776+cyberpunk9932@users.noreply.github.com>
2024-06-24 18:27:40 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ const frameMetadata = getFrameMetadata({
     aspectRatio: '1:1',
   },
   input: {
-    text: 'Tell me a story about your enexpected farts in a public place.',
+    text: 'Tell a story about farts in a public place.',
   },
   postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
 });

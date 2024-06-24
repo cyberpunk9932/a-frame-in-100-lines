d6c1a47fefee887460e2ff6e1641dd1aa9123830
cyberpunk9932 <70695776+cyberpunk9932@users.noreply.github.com>
2024-06-24 18:07:26 +0500

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,4 @@
 // use NODE_ENV to not have to change config based on where it's deployed
-export const NEXT_PUBLIC_URL = 'https://a-frame-in-100-lines-qt2g3l1kt-cyberpunk9932s-projects.vercel.app/'
+export const NEXT_PUBLIC_URL = 'https://a-frame-in-100-lines-qt2g3l1kt-cyberpunk9932s-projects.vercel.app'
   process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://zizzamia.xyz';
 export const BUY_MY_COFFEE_CONTRACT_ADDR = '0x575f1Ca07Ff1958d30798b9EBA9210C10fe1Be03';
